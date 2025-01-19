e5d3035e9bd9a3d382d165f17c97720a22af5266
SkriZZ <1roman99@mail.ru>
2025-01-20 00:42:26 +0300

    На ревью

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/SkriZZ/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки 
+function createСard(Data) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true),
+    cardImage = cardElement.querySelector(".card__image"),
+    cardTitle = cardElement.querySelector(".card__title"),
+    deleteButton = cardElement.querySelector(".card__delete-button");
 
+    cardImage.setAttribute("src", Data.link);
+    cardImage.setAttribute("alt", Data.name);
+    cardTitle.textContent = Data.name;
+
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement
+}
 // @todo: Функция удаления карточки
-
+    function deleteCard(event) {
+    event.target.closest('.card').remove();
+    }
 // @todo: Вывести карточки на страницу
+    initialCards.forEach((Data) => {
+    const cardElement = createСard(Data);
+    placesList.append(cardElement);
+  });
